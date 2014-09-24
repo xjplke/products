6338e81c5a9a27794c514231194580304d1bb8b5
root <root@centos64.(none)>
2014-09-25 01:28:20 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cp ./repos/*.repo /etc/yum.repos.d/
+
+#cache rpms
+sed -i "s/keepcache=0/keepcache=1/g" /etc/yum.conf 
+
+lastrpm=`ls -t | grep adfi | head -1`
+
+yum localinstall ./${lastrpm}
+
+/etc/init.d/iptables stop
+
+
